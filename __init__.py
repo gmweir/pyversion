685dfa00d8f86caf577017d9ea361a1e0053ae3d
Gavin Weir <gavin.weir@ipp.mpg.de>
2016-09-08 14:14:23 +0200

    basic init file

--- __init__.py
+++ __init__.py
@@ -2,6 +2,17 @@
 """
 Created on Fri Aug 12 12:23:01 2016
 
-@author: gawe
-"""
+==========
+@authors: gavin.weir@ipp-hgw.mpg.de
+@copyright: 2016
+@license: GNU GPL
 
+@author: G.M. Weir
+"""
+###############################################################################
+###############################################################################
+
+__version__ = "2016.08.15.12"
+__all__ = ['version']
+
+from . import version
